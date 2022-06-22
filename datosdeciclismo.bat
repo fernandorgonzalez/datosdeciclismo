51d90b4016894ca0234d919444b4e135c7dbc29c
Fernando Gonzalez <fernandorgonzalez@outlook.com>
2022-06-22 18:40:30 -0300

    Updating .bat file

--- datosdeciclismo.bat
+++ datosdeciclismo.bat
@@ -1,4 +1,4 @@
-call "C:\Users\Fernando Gonzalez\anaconda3\Scripts\activate.bat"
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
 call python etl_01.py
 call python etl_02.py
 call python etl_03.py
